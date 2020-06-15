63df6cc05319b26024491a56b99e571fe949dd3a
Takashi Kanemoto <ttskch@gmail.com>
2020-06-16 01:57:19 +0900

    feat: 🎸 期日超過のタスクを強調表示するように

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   tasks = [
     {title: '牛乳を買う', done: false, deadline: new Date('2021-01-01')},
-    {title: '可燃ゴミを出す', done: true, deadline: new Date('2021-01-02')},
-    {title: '銀行に行く', done: false, deadline: new Date('2021-01-03')},
+    {title: '可燃ゴミを出す', done: true, deadline: new Date('2020-01-02')},
+    {title: '銀行に行く', done: false, deadline: new Date('2020-01-03')},
   ];
 
   newTask = {
@@ -23,5 +23,9 @@ export class AppComponent {
       title: '',
       deadline: new Date(),
     };
+  }
+
+  isOverdue(task) {
+    return !task.done && task.deadline < (new Date()).setHours(0, 0, 0, 0);
   }
 }
