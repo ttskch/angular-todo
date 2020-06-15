d4563a312be488fb6530561fe0fb9a3fee0985e3
Takashi Kanemoto <ttskch@gmail.com>
2020-06-16 04:04:24 +0900

    refactor: 💡 タスク追加フォームをTaskFormComponentに移植

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-task-form',
@@ -9,7 +9,21 @@ export class TaskFormComponent implements OnInit {
 
   constructor() { }
 
+  @Output() addTask = new EventEmitter();
+
+  newTask = {
+    title: '',
+    deadline: new Date(),
+  };
+
   ngOnInit(): void {
   }
 
+  submit() {
+    this.addTask.emit({title: this.newTask.title, done: false, deadline: new Date(this.newTask.deadline)});
+    this.newTask = {
+      title: '',
+      deadline: new Date(),
+    };
+  }
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -15,19 +15,10 @@ export class TaskListComponent implements OnInit {
     {title: '銀行に行く', done: false, deadline: new Date('2020-01-03')},
   ];
 
-  newTask = {
-    title: '',
-    deadline: new Date(),
-  };
-
   ngOnInit(): void {
   }
 
-  addTask() {
-    this.tasks.push({title: this.newTask.title, done: false, deadline: new Date(this.newTask.deadline)});
-    this.newTask = {
-      title: '',
-      deadline: new Date(),
-    };
+  addTask(task) {
+    this.tasks.push(task);
   }
 }
