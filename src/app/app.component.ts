410d8841010d54e0f4e1a51893e2698049e2ecfd
Takashi Kanemoto <ttskch@gmail.com>
2020-06-15 18:06:43 +0900

    feat: 🎸 コンポーネントの変数をテンプレートに表示してみる

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  tasks = [
+    {title: '牛乳を買う', done: false},
+    {title: '可燃ゴミを出す', done: true},
+    {title: '銀行に行く', done: false},
+  ];
 }
